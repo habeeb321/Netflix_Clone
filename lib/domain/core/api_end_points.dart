847bca75ef1a14c0bae9877632ba192b212d5c1f
habeeb321 <habeebrh321@gmail.com>
2022-11-23 04:48:04 +0530

    Models created for api integration

---
+++ lib/domain/core/api_end_points.dart
@@ -0,0 +1,6 @@
+import 'package:netflix_clone_app/core/strings.dart';
+import 'package:netflix_clone_app/infrastucture/api_key.dart';
+
+class ApiEndPoints {
+  static const downloads = "$kBaseUrl/trending/all/day?api_key=$apiKey";
+}
